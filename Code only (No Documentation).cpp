a2707588be000b303e23e79145219bf579eace06
FidelisMarcelino <159218154+FidelisMarcelino@users.noreply.github.com>
2024-05-10 16:49:50 +0700

    fourth commit

--- Code only (No Documentation).cpp
+++ Code only (No Documentation).cpp
@@ -6,9 +6,8 @@
 #define CHAR_SIZE 256
 
 int count = 1;
-char word[101];
 
-struct descNode{
+struct descNode{			
 	char description[100];
 	struct descNode *next;
 };
@@ -43,6 +42,7 @@ struct descNode *createDescNode(char *desc){
 void insert(struct trieNode *root, char *key, char *desc){
 	struct trieNode *current = root;
 	int length = strlen(key);
+	
 	for(int i = 0; i < length; i++){
 		int index = key[i] - 'A';
 		if(!current->children[index]){
@@ -50,8 +50,11 @@ void insert(struct trieNode *root, char *key, char *desc){
 		}
 		current = current->children[index];
 	}
+	
 	current->isEndOfWord = true;
+	
 	struct descNode *newDescNode = createDescNode(desc);
+	
 	if(!current->descHead){
 		current->descHead = newDescNode;
 	} else {
@@ -61,11 +64,14 @@ void insert(struct trieNode *root, char *key, char *desc){
 		}
 		temp->next = newDescNode;
 	}
+	
+	strcpy(current->descHead->description, desc);
 }
 
 char *searchDesc(struct trieNode *root, char *key){
 	struct trieNode *current = root;
 	int length = strlen(key);
+	
 	for(int i = 0; i < length; i++){
 		int index = key[i] - 'A';
 		if(!current->children[index]){
@@ -73,6 +79,7 @@ char *searchDesc(struct trieNode *root, char *key){
 		}
 		current = current->children[index];
 	}
+	
 	if(current != NULL && current->isEndOfWord){
 		if(current->descHead != NULL){
 			return current->descHead->description;
@@ -84,6 +91,7 @@ char *searchDesc(struct trieNode *root, char *key){
 
 void printTrie(struct trieNode *node, char *buffer, int depth){
 	bool hasWords = false;
+	
 	if(node->isEndOfWord){
 		hasWords = true;
 		if(count == 1){
@@ -93,6 +101,7 @@ void printTrie(struct trieNode *node, char *buffer, int depth){
 		printf("%d. %s\n",count, buffer);
 		count++;
 	}
+	
 	for(int i = 0; i < CHAR_SIZE; i++){
 		if(node->children[i] != NULL){
 			buffer[depth] = i + 'A';
@@ -100,6 +109,7 @@ void printTrie(struct trieNode *node, char *buffer, int depth){
 			hasWords = true;
 		}
 	}
+	
 	if(!hasWords){
 		printf("There is no slang words yet in the dictionary.\n");
 	}
@@ -107,6 +117,7 @@ void printTrie(struct trieNode *node, char *buffer, int depth){
 
 void printTrieWithPrefix(struct trieNode *root, char *prefix, char *buffer, int depth){
 	bool hasWords = false;
+	
 	if(root->isEndOfWord){
 		hasWords = true;
 		if(count == 1){
@@ -116,6 +127,7 @@ void printTrieWithPrefix(struct trieNode *root, char *prefix, char *buffer, int 
 		printf("%d. %s%s\n", count, prefix, buffer);
 		count++;
 	}
+	
 	for(int i = 0; i < CHAR_SIZE; i++){
 		if(root->children[i] != NULL){
 			buffer[depth] = i + 'A';
@@ -123,6 +135,7 @@ void printTrieWithPrefix(struct trieNode *root, char *prefix, char *buffer, int 
 			hasWords = true;
 		}
 	}
+	
 	if(!hasWords){
 		printf("There is no prefix \"%s\" in the dictionary.\n", prefix);
 	}
@@ -132,7 +145,9 @@ void searchPrefix(struct trieNode *root, char *prefix){
 	struct trieNode *current = root;
 	int length = strlen(prefix);
 	char buffer[100];
+	
 	count = 1;
+	
 	for(int i = 0; i < length; i++){
 		int index = prefix[i] - 'A';
 		if(current->children[index] == NULL){
@@ -141,6 +156,7 @@ void searchPrefix(struct trieNode *root, char *prefix){
 		}
 		current = current->children[index];
 	}
+	
 	printf("Words starting with \"%s\": \n", prefix);
 	printTrieWithPrefix(current, prefix, buffer, 0);
 }
@@ -169,53 +185,74 @@ int wordReq(char *str){
 int main(){
 	struct trieNode *root = createNode();
 	int choice;
-	char desc[100], searchWord[100], searchPrefix1[100];
+	char word[100], desc[100], searchWord[100], searchPrefix1[100];
+	
 	while(true){
 		menu();
 		scanf("%d", &choice);
+		
 		switch(choice){
 			case 1:{
+			
 				printf("Input a new slang word [Must be more than 1 characters and contains no space]: ");
 				scanf("%s", word); getchar();
+				
 				while(strlen(word) <= 1 && strchr(word, ' ') == 0){
 					printf("Input a new slang word [Must be more than 1 characters and contains no space]: ");
 					scanf("%s", word); getchar();
-				}
+				} 
+				
 				printf("Input a new slang word description [Must be more than 2 words ]: ");
 				scanf("%[^\n]", desc); getchar();
-				while(wordReq(desc) < 1){
+					
+				while(wordReq(desc) < 2){									
 					printf("Input a new slang word description [Must be more than 2 words ]: ");
 					scanf("%[^\n]", desc); getchar();
 				}
+								
+				if(searchDesc(root, word)){
+					printf("Successfully updated a slang word.\n");
+				} else {
+					printf("Successfully released new slang word.\n");
+				}
+				
 				insert(root, word, desc);
-				printf("Succesfully released new slang word.\n");
+				
 				system("pause");
 				break;
 			}
+			
 			case 2:{
 				printf("Input a slang word to be searched [Must be more than 1 characters and contains no space]: ");
 				scanf("%s", searchWord); getchar();
+				
 				while(strlen(searchWord) < 2 && strchr(searchWord, ' ') == 0){
 					printf("Input a slang word to be searched [Must be more than 1 characters and contains no space]: ");
 					scanf("%s", searchWord); getchar();
 				}
+				
 				if(!searchDesc(root, searchWord)){
 					printf("There is no word '%s' in the dictionary.\n", searchWord);
 				} else {
 					printf("Slang word: %s\n", searchWord);
+					
 					char *wordDesc = searchDesc(root, searchWord);
+					
 					printf("Description: %s\n", wordDesc);
 				}
 				system("pause");
 				break;
 			}
+			
 			case 3:{
 				printf("Input a prefix to be searched: ");
 				scanf("%s", searchPrefix1); getchar();
+				
 				searchPrefix(root, searchPrefix1);
 				system("pause");
 				break;
 			}
+			
 			case 4:{
 				count = 1;
 				char buffer[100];
@@ -223,6 +260,7 @@ int main(){
 				system("pause");
 				break;
 			}
+			
 			case 5:{
 				printf("Thank you... Have a nice day :)\n");
 				return 0;
@@ -230,6 +268,7 @@ int main(){
 			}
 		}
 	}
+	
 	return 0;
 }
 
